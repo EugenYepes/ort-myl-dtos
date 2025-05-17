c4b6a9d667f79872a49b158291b42ddca82a8490
EugenYepes <eugenionyepes@gmail.com>
2025-05-17 18:15:57 -0300

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,7 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
+version = "1.0"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
